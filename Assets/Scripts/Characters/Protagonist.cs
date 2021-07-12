16d5c6d3eec51ecb12e14f4c0b6d2500c1bf339f
eungyukm <86348187+eungyukm@users.noreply.github.com>
2021-07-12 16:05:03 +0900

    Add project

---
+++ Assets/Scripts/Characters/Protagonist.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// <para>This component consumes input on the InputReader and stores its values. The input is then read, and manipulated, by the StateMachines's Actions.</para>
+/// </summary>
+public class Protagonist : MonoBehaviour
+{
+	[SerializeField] private InputReader _inputReader = default;
+	public Transform gameplayCamera;
+
+	private Vector2 _previousMovementInput;
+
+	//These fields are read and manipulated by the StateMachine actions
+	[HideInInspector] public bool jumpInput;
+	[HideInInspector] public bool extraActionInput;
+	[HideInInspector] public Vector3 movementInput; //Initial input coming from the Protagonist script
+	[HideInInspector] public Vector3 movementVector; //Final movement vector, manipulated by the StateMachine actions
+	[HideInInspector] public ControllerColliderHit lastHit;
+
+	private void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		lastHit = hit;
+	}
+
+	//Adds listeners for events being triggered in the InputReader script
+	private void OnEnable()
+	{
+		_inputReader.jumpEvent += OnJumpInitiated;
+		_inputReader.jumpCanceledEvent += OnJumpCanceled;
+		_inputReader.moveEvent += OnMove;
+		_inputReader.extraActionEvent += OnExtraAction;
+		//...
+	}
+
+	//Removes all listeners to the events coming from the InputReader script
+	private void OnDisable()
+	{
+		_inputReader.jumpEvent -= OnJumpInitiated;
+		_inputReader.jumpCanceledEvent -= OnJumpCanceled;
+		_inputReader.moveEvent -= OnMove;
+		_inputReader.extraActionEvent -= OnExtraAction;
+		//...
+	}
+
+	private void Update()
+	{
+		RecalculateMovement();
+	}
+
+	private void RecalculateMovement()
+	{
+		//Get the two axes from the camera and flatten them on the XZ plane
+		Vector3 cameraForward = gameplayCamera.forward;
+		cameraForward.y = 0f;
+		Vector3 cameraRight = gameplayCamera.right;
+		cameraRight.y = 0f;
+
+		//Use the two axes, modulated by the corresponding inputs, and construct the final vector
+		Vector3 adjustedMovement = cameraRight.normalized * _previousMovementInput.x +
+			cameraForward.normalized * _previousMovementInput.y;
+
+		movementInput = Vector3.ClampMagnitude(adjustedMovement, 1f);
+	}
+
+	//---- EVENT LISTENERS ----
+
+	private void OnMove(Vector2 movement)
+	{
+		_previousMovementInput = movement;
+	}
+
+	private void OnJumpInitiated()
+	{
+		jumpInput = true;
+	}
+
+	private void OnJumpCanceled()
+	{
+		jumpInput = false;
+	}
+
+	// This handler is just used for debug, for now
+	private void OnExtraAction()
+	{
+		extraActionInput = true;
+	}
+}
